60d66ecdb151867acbed079d1ed0ae029d37819e
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-12-28 09:17:32 -0300

    Criar model Colaborador

--- src/main/java/FolhaPag/models/Colaborador.java
+++ src/main/java/FolhaPag/models/Colaborador.java
@@ -2,12 +2,18 @@ package FolhaPag.models;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "colaboradores")
 public class Colaborador {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String nomeColaborador;
+    @Column(nullable = false)
+    private String CPF;
+    @Column(nullable = false)
+    private double salarioBase;
 }
