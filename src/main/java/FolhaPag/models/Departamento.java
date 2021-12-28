adab7fbd29d1f2ead2f6e3306282072ebb299137
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-12-28 09:20:53 -0300

    Criar model Departamento

---
+++ src/main/java/FolhaPag/models/Departamento.java
@@ -0,0 +1,17 @@
+package FolhaPag.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "departamentos")
+public class Departamento {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, unique = true)
+    private String nomeDepartamento;
+    private List<Colaborador> colaboradores;
+}
