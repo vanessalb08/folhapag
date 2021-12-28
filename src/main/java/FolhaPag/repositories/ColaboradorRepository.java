30be9778a24fb40546480661c9c605f7a8a9b91b
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-12-28 09:32:01 -0300

    Criar repositories Departamento e Colaborador

---
+++ src/main/java/FolhaPag/repositories/ColaboradorRepository.java
@@ -0,0 +1,7 @@
+package FolhaPag.repositories;
+
+import FolhaPag.models.Colaborador;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ColaboradorRepository extends CrudRepository<Colaborador, Integer> {
+}
---
+++ src/main/java/FolhaPag/repositories/DepartamentoReposoiitory.java
@@ -0,0 +1,7 @@
+package FolhaPag.repositories;
+
+import FolhaPag.models.Departamento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DepartamentoReposoiitory extends CrudRepository<Departamento, Integer> {
+}
